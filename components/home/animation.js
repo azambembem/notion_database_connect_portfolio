23f819f68bd43725421bb60c9fa7499df2dd70d4
azambembem <azamjan980713@gmail.com>
2024-12-18 15:58:45 +0900

    add code

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,17 +1,42 @@
-import React from "react";
+// import React from "react";
+// import Lottie from "react-lottie-player";
+
+// // import lottieJson from "/public/animation.json";
+// // import lottieJson from "/animation.json";
+// import lottieJson from "../../public/animation.json";
+
+// export default function Animation() {
+//   return (
+//     <Lottie
+//       loop
+//       animationData={lottieJson}
+//       play
+//       style={{ width: `100%`, height: `100%` }}
+//     />
+//   );
+// }
+
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie-player";
 
-// import lottieJson from "/public/animation.json";
-// import lottieJson from "/animation.json";
-import lottieJson from "/public/Animation.json";
-
 export default function Animation() {
+  const [lottieJson, setLottieJson] = useState(null);
+
+  useEffect(() => {
+    // public/animation.json 파일을 fetch
+    fetch("/animation.json")
+      .then((response) => response.json())
+      .then((data) => setLottieJson(data));
+  }, []);
+
+  if (!lottieJson) return null; // 데이터를 가져오기 전에는 렌더링하지 않음
+
   return (
     <Lottie
       loop
       animationData={lottieJson}
       play
-      style={{ width: `100%`, height: `100%` }}
+      style={{ width: "100%", height: "100%" }}
     />
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/layout";
 import Hero from "../components/home/hero";
-import Animation from "../components/home/animation.js";
+// import Animation from "../components/home/animation.js";
 // import Animation from "../components/home/animation.js";
 
 export default function Home() {
