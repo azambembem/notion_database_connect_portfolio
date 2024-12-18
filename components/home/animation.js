08aef01bac3c1700d68bf2ab70b1d7d373fa3084
azambembem <azamjan980713@gmail.com>
2024-12-18 17:45:02 +0900

    change code commit

--- components/home/animation.js
+++ components/home/animation.js
@@ -1,15 +1,42 @@
-import React from "react";
+// import React from "react";
+// import Lottie from "react-lottie-player";
+
+// // import lottieJson from "/animation.json";
+// import lottieJson from "/public/animation.json";
+// // import lottieJson from "/animation.json";
+// // import lottieJson from "../../public/animation.json";
+
+// export default function Animation() {
+//   return (
+//     <Lottie
+//       loop
+//       animationData={lottieJson}
+//       play
+//       style={{ width: `100%`, height: `100%` }}
+//     />
+//   );
+// }
+
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie-player";
 
-import lottieJson from "/public/animation.json";
-// import lottieJson from "/animation.json";
-// import lottieJson from "../../public/animation.json";
-
 export default function Animation() {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch("/animation.json") // public 폴더에 있는 JSON 파일을 fetch로 불러옴
+      .then((response) => response.json())
+      .then((data) => setAnimationData(data));
+  }, []);
+
+  if (!animationData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Lottie
       loop
-      animationData={lottieJson}
+      animationData={animationData}
       play
       style={{ width: `100%`, height: `100%` }}
     />
