4fe44b5bd4ec71b3fa386928bcff3acfe498aa68
azambembem <azamjan980713@gmail.com>
2024-12-18 20:45:38 +0900

    change code

--- components/home/animation.js
+++ components/home/animation.js
@@ -17,17 +17,53 @@
 //   );
 // }
 
+// import React, { useEffect, useState } from "react";
+// import Lottie from "react-lottie-player";
+
+// export default function Animation() {
+//   const [animationData, setAnimationData] = useState(null);
+
+//   useEffect(() => {
+//     fetch("/animation.json") // public 폴더에 있는 JSON 파일을 fetch로 불러옴
+//       .then((response) => response.json())
+//       .then((data) => setAnimationData(data));
+//   }, []);
+
+//   if (!animationData) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <Lottie
+//       loop
+//       animationData={animationData}
+//       play
+//       style={{ width: `100%`, height: `100%` }}
+//     />
+//   );
+// }
+
 import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie-player";
 
 export default function Animation() {
   const [animationData, setAnimationData] = useState(null);
+  const [isBrowser, setIsBrowser] = useState(false);
 
   useEffect(() => {
-    fetch("/animation.json") // public 폴더에 있는 JSON 파일을 fetch로 불러옴
-      .then((response) => response.json())
-      .then((data) => setAnimationData(data));
+    // 브라우저 환경 확인
+    if (typeof window !== "undefined") {
+      setIsBrowser(true);
+
+      fetch("/animation.json") // public 폴더에 있는 JSON 파일을 fetch로 불러옴
+        .then((response) => response.json())
+        .then((data) => setAnimationData(data));
+    }
   }, []);
+
+  if (!isBrowser) {
+    return null; // SSR 환경에서는 아무것도 렌더링하지 않음
+  }
 
   if (!animationData) {
     return <div>Loading...</div>;
